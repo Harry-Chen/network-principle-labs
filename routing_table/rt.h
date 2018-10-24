ab1f344ee0cdfbd57da8d7057a415481f05ae99d
Harry Chen <i@harrychen.xyz>
2018-10-24 17:56:14 +0800

    Update header file for routing_table
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- routing_table/rt.h
+++ routing_table/rt.h
@@ -3,9 +3,13 @@
 
 #include <stdint.h>
 
+// You should use host byte order for all ips passed to these functions.
+// Insertion and removal will not fail in any cases.
+// If lookup fails, 0 will be returned.
+
 extern void *rt_init();
 extern void rt_insert(void *table, uint32_t ip, uint32_t prefix, uint32_t index);
 extern void rt_remove(void *table, uint32_t ip, uint32_t prefix);
 extern uint32_t rt_lookup(void *table, uint32_t ip);
 
-#endif
+#endif // __RT_H__
